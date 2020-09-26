cbfc84d628e07565963b1a7d9b258846510e7683
Octavio Carpes <octavio.carpes10@gmail.com>
2020-09-26 15:31:04 -0300

    Deleted unnecessary code

--- AdditionInterfaceClient.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface AdditionInterfaceClient extends Remote {
-	public String Result(String val) throws RemoteException;
-}
--- AdditionInterfaceServer.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface AdditionInterfaceServer extends Remote {
-	public void Add() throws RemoteException;
-}
